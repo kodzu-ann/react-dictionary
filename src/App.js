6acbae946fec37535b47671e0c1e872bf655b4e3
Kodzu-ann <ann.cooper2023@gmail.com>
2022-11-06 12:32:33 +0100

    add dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header ">HELLO</header>
+      <div className="containe">
+        <header className="App-header ">HELLO</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer text-center">
+          Coded by <a href="https://github.com/kodzu-ann"> kodzu-ann </a>
+        </footer>
+      </div>
     </div>
   );
 }
